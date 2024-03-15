d87586cfbe98b194005dea9065bbbbb238c311ed
yeri <3477kim@naver.com>
2024-03-15 16:06:53 +0900

    2024-03-15

--- exercise11/webapp/controller/Main.controller.js
+++ exercise11/webapp/controller/Main.controller.js
@@ -20,6 +20,21 @@ sap.ui.define([
 
                 // 해당 Model 내용의 Carrid 속성을 화면에 출력
                 sap.m.MessageToast.show(oContext.getProperty("Carrid"));
+
+                // 선택한 라인의 접근 경로
+                let selectedPath = oContext.getPath();
+
+                // 항공편이 보이는 Table의 ID로 화면의 구성요소를 찾는다.
+                let oConnTable = this.byId("idConnTable");
+
+                // 선택한 라인의 접근 경로를 현재 경로로 취급하도록
+                // 테이블의 bindElement 메소드를 호출한다.
+                // 이렇게 테이블에서 현재 경로를 설정하면
+                // items 에 적은 {toConnection} 은 
+                // {선택한항공사/toConnection} 이라고 적은 것과 같도록 된다.
+                //{/CarrierSet('AA')/ 경로에서 {toConnection}를 찾아라}
+                oConnTable.bindElement(selectedPath);
+                
             }
         });
     });
