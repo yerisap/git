fe7de1f2d5ca4fa0298b4f70d0cd0edd4950e2f5
yeri <3477kim@naver.com>
2024-03-25 12:37:35 +0900

    시험리뷰리

---
+++ ux1review/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["sync/e08/ux1review/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ ux1review/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"synce08/ux1review/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
